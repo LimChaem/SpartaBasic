fdf2f4166703685600a311efda6057d122f0be67
Chaemyung Lim <saver203@naver.com>
2024-03-29 16:34:54 +0900

    binding

--- app/src/main/java/com/lim/basic_assignment/MainActivity.kt
+++ app/src/main/java/com/lim/basic_assignment/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.lifecycleScope
+import com.lim.basic_assignment.databinding.ActivityMainBinding
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.isActive
@@ -18,56 +19,50 @@ import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     private var job: Job? = null
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
         setupButton()
         setRandomValueBetweenOneToHundred()
         setJobAndLaunch()
     }
 
-
     private fun setupButton() {
-        val button = findViewById<Button>(R.id.clickButton)
-        button.setOnClickListener {
+        binding.clickButton.setOnClickListener {
             job?.cancel()
             checkAnswerAndShowToast()
         }
     }
 
     private fun setRandomValueBetweenOneToHundred() {
-        val randomTextView = findViewById<TextView>(R.id.textViewRandom)
-        val random = (1..100).random() // Random.nextInt(1 .. 100) or (1..100).random()
-
-        // val random = Random
-        // val randomValue = random.nextInt(1,100)
-        // randomTextView.text = randomValue.toString()
-
-        randomTextView.text = random.toString()
+        val random = (1..100).random()
+        binding.textViewRandom.text = random.toString()
 
     }
 
     private fun setJobAndLaunch() {
-        val textView = findViewById<TextView>(R.id.spartaTextView)
-        /* 피드백 수정 -> job?.cancel()을 주지 않을 경우 이전 작업이 유지된 상태에서 새로운 작업 시작하므로
-         메모리에 쌓이게 되어 이전 작업을 취소 하고 새로운 작업을 시작 해야 한다?*/
+
         job?.cancel()
         job = lifecycleScope.launch {
             for (i in 1..100) {
                 if (isActive) {
-                    textView.text = i.toString()
+                    binding.spartaTextView.text = i.toString()
                     delay(100)
 
                     if (i == 100) {
-                        /* job?.cancel()를 이 위치에 넣어도 작업이 완료된 후에 다시 시작 하기 전에 작업을 취소 하고
-                         setJobAndLaunch()가 시작 되면서 새로운 작업이 시작 되지 않을까? */
-                        setJobAndLaunch()       // 100이 됐을 때 다시 1부터 시작 하기.
+                        setJobAndLaunch()
                     }
                 }
             }
@@ -75,14 +70,11 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun checkAnswerAndShowToast() {
-        val textView = findViewById<TextView>(R.id.spartaTextView)
-        val randomTextView = findViewById<TextView>(R.id.textViewRandom)
 
-        if (textView.text == randomTextView.text) {
+        if (binding.spartaTextView.text.toString() == binding.textViewRandom.text.toString()) {
             Toast.makeText(this, "숫자가 일치합니다.", Toast.LENGTH_SHORT).show()
             val intent = Intent(this, SecondActivity::class.java)
-            val count = textView.text.toString()
-            intent.putExtra("Count", count) // Intent 사용에 익숙해지기 위해 사용해봄.
+            intent.putExtra("Count", binding.spartaTextView.text.toString())
             startActivity(intent)
         } else {
             Toast.makeText(this, "숫자가 일치하지 않습니다.", Toast.LENGTH_SHORT).show()
--- app/src/main/java/com/lim/basic_assignment/SecondActivity.kt
+++ app/src/main/java/com/lim/basic_assignment/SecondActivity.kt
@@ -8,12 +8,17 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.lim.basic_assignment.databinding.ActivityMainBinding
+import com.lim.basic_assignment.databinding.ActivitySecondBinding
 
 class SecondActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySecondBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_second)
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -21,16 +26,12 @@ class SecondActivity : AppCompatActivity() {
         }
 
         val count = intent.getStringExtra("Count")
-        val countText = findViewById<TextView>(R.id.tv_count)
-        countText.text = count
+        if(count != null) {
+            binding.tvText.text = count
+        }
 
-        val btn_back = findViewById<Button>(R.id.btn_back)
-        btn_back.setOnClickListener {
+        binding.btnBack.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
-
-            // startActivity(intent) 만으로도 MainActivity로 이동이 가능함
-            // 다만, 그럴 경우엔 기존의 MainActivitiy는 살아있고, 그 위에 차곡 차곡 쌓인다고 함.
-            // FLAG_ACTIVITY_CLEAR_TOP를 사용하면 액티비티 이동 시 이동하려는 Activity를 최상단으로 올리고 나머지는 모두 삭제.
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
             startActivity(intent)
             finish()
