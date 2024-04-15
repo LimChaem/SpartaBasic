c3dc03b6dba7615cb2f40471e3af7bff4361e68e
Chaemyung Lim <saver203@naver.com>
2024-04-15 16:50:17 +0900

    test1

--- app/src/main/java/com/example/sparta_basic_week4/SecondActivity.kt
+++ app/src/main/java/com/example/sparta_basic_week4/SecondActivity.kt
@@ -1,15 +1,34 @@
 package com.example.sparta_basic_week4
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.sparta_basic_week4.databinding.ActivityMainBinding
+import com.example.sparta_basic_week4.databinding.ActivitySecondBinding
 
 class SecondActivity : AppCompatActivity() {
     private val TAG = "SecondActivity"
+    private lateinit var binding:ActivitySecondBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.i(TAG, "onCreate")
+        enableEdgeToEdge()
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
 
+        binding.backButton.setOnClickListener {
+            finish()
+        }
     }
 
     override fun onRestart() {
